52953e93f69d7abd0a6089f96b1759d40acc2b39
adarsh251 <mishraadarsh25104@gmail.com>
2024-07-24 18:56:17 +0530

    server static file using path.join

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3600;
 app.use(bodyParser.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname,"public")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
