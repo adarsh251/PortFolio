9f239e6d8c4a08f01810f789319b23c04dbeed4c
adarsh251 <mishraadarsh25104@gmail.com>
2025-03-08 16:35:03 +0530

    Environment variables update

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ app.post("/verify-recaptcha", async (req, res) => {
     secret : process.env.SECRET_KEY,
     response : req.body['g-recaptcha-response']
   })
-  console.log(req.body);
   const response = await fetch("https://www.google.com/recaptcha/api/siteverify", {
     method: "POST",
     body: params
